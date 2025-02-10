3be7bba07aa89520a5bf131fbdfbbf756e54e4a6
mule991 <mulemuler@gmail.com>
2025-02-10 07:53:31 -0500

    header and footer

---
+++ netflix-clone/src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../../componenets/Header/Header'
+import Footer from '../../componenets/Footer/Footer'
+
+function Home() {
+  return (
+   <>
+   <Header/>
+   <Footer/>
+   </>
+  )
+}
+
+export default Home
